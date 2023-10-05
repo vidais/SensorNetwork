f71ee803ad78181aeba8bd77af5196ab6e9ab272
António <antonio.vidais@tecnico.ulisboa.pt>
2023-10-05 16:28:37 +0100

    2 main activites are mostly designed but have no funcionality

---
+++ app/src/main/java/sensorNetwork/part1/TemperatureActivity.java
@@ -0,0 +1,11 @@
+package sensorNetwork.part1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class TemperatureActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sensor_data);
+    }
+}
